8688ae6731ae29aaf1a235a73f94b4bfb5884b57
jiffylubeyou <59983470+jiffylubeyou@users.noreply.github.com>
2022-10-31 19:04:57 -0600

    The scoring is workinig I'm pretty sure

--- GeneSequencing.py
+++ GeneSequencing.py
@@ -33,9 +33,24 @@ class GeneSequencing:
 		self.banded = banded
 		self.MaxCharactersToAlign = align_length
 
+		seq1Array = []
+		seq1Array.append(None)
+		seq2Array = []
+		seq2Array.append(None)
+		for char in seq1:
+			seq1Array.extend(char)
+		for char in seq2:
+			seq2Array.extend(char)
+		E = [[Node() for x in range(len(seq2Array))] for y in range(len(seq1Array))]
+		Edit(seq1Array, seq2Array, E)
+
+
+
+
+
 ###################################################################################################
 # your code should replace these three statements and populate the three variables: score, alignment1 and alignment2
-		score = random.random()*100;
+		score = E[-1][-1].data
 		alignment1 = 'abc-easy  DEBUG:({} chars,align_len={}{})'.format(
 			len(seq1), align_length, ',BANDED' if banded else '')
 		alignment2 = 'as-123--  DEBUG:({} chars,align_len={}{})'.format(
@@ -45,3 +60,50 @@ class GeneSequencing:
 		return {'align_cost':score, 'seqi_first100':alignment1, 'seqj_first100':alignment2}
 
 
+def Edit(x, y, E):
+	for i in range(len(x)):
+		E[i][0].data = i
+	for j in range(len(y)):
+		E[0][j].data = j
+	for i in range(1, len(x)):
+		for j in range(1, len(y)):
+			subNumber = 0
+			if x[i] == y[j]:
+				subNumber = E[(i-1)][(j-1)].data - 3
+			else:
+				subNumber = E[(i-1)][(j-1)].data + 1
+			addNumber = E[i][j - 1].data + 5
+			deleteNumber = E[i - 1][j].data + 5
+			# This finds the minimum of the three
+			minimum = subNumber
+			if addNumber < minimum:
+				minimum = addNumber
+			if deleteNumber < minimum:
+				minimum = deleteNumber
+			# You have minimum number now write in number
+			if minimum == subNumber:
+				E[i][j].data = subNumber
+				E[i][j].backPath = E[i - 1][j - 1]
+				E[i][j].backDirection = "diagonal"
+			elif minimum == addNumber:
+				E[i][j].data = addNumber
+				E[i][j].backPath = E[i][j - 1]
+				E[i][j].backDirection = "left"
+			elif minimum == deleteNumber:
+				E[i][j].data = deleteNumber
+				E[i][j].backPath = E[i - 1][j]
+				E[i][j].backDirection = "up"
+			else:
+				print("Something ain't right here")
+
+
+
+
+
+
+
+class Node(object):
+	def __init__(self):
+		self.data = 0
+		self.backPath = None
+		self.backDirection = None
