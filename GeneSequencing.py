892ee2df4f3922277a24e8c8b08087906a5065bc
jiffylubeyou <59983470+jiffylubeyou@users.noreply.github.com>
2022-11-01 19:17:17 -0600

    working final without banded

--- GeneSequencing.py
+++ GeneSequencing.py
@@ -41,9 +41,30 @@ class GeneSequencing:
 			seq1Array.extend(char)
 		for char in seq2:
 			seq2Array.extend(char)
+		# Code below will determine if E is align length or string length
+		if align_length < len(seq1Array):
+			seq1Array = seq1Array[0:align_length + 1]
+
+		if align_length < len(seq2Array):
+			seq2Array = seq2Array[0:align_length + 1]
+
 		E = [[Node() for x in range(len(seq2Array))] for y in range(len(seq1Array))]
 		Edit(seq1Array, seq2Array, E)
 
+		alignment1 = ''
+		alignment2 = ''
+		tempNode = E[-1][-1]
+		while (tempNode != E[0][0]):
+			if tempNode.backDirection == "diagonal":
+				alignment1 = seq1Array[tempNode.coord[0]] + alignment1
+				alignment2 = seq2Array[tempNode.coord[1]] + alignment2
+			if tempNode.backDirection == "up":
+				alignment1 = seq1Array[tempNode.coord[0]] + alignment1
+				alignment2 = '-' + alignment2
+			if tempNode.backDirection == "left":
+				alignment1 = '-' + alignment1
+				alignment2 = seq2Array[tempNode.coord[1]] + alignment2
+			tempNode = tempNode.backPath
 
 
 
@@ -51,20 +72,30 @@ class GeneSequencing:
 ###################################################################################################
 # your code should replace these three statements and populate the three variables: score, alignment1 and alignment2
 		score = E[-1][-1].data
-		alignment1 = 'abc-easy  DEBUG:({} chars,align_len={}{})'.format(
-			len(seq1), align_length, ',BANDED' if banded else '')
-		alignment2 = 'as-123--  DEBUG:({} chars,align_len={}{})'.format(
-			len(seq2), align_length, ',BANDED' if banded else '')
+		# alignment1 = alignment1.format(
+		# 	len(seq1), align_length, ',BANDED' if banded else '')
+		# alignment2 = 'as-123--  DEBUG:({} chars,align_len={}{})'.format(
+		# 	len(seq2), align_length, ',BANDED' if banded else '')
 ###################################################################################################					
 		
 		return {'align_cost':score, 'seqi_first100':alignment1, 'seqj_first100':alignment2}
 
 
 def Edit(x, y, E):
+	# 0 out the first rows and columns
 	for i in range(len(x)):
 		E[i][0].data = i
+		E[i][0].coord = [i, 0]
+		if (i != 0):
+			E[i][0].backPath = E[i - 1][0]
+		E[i][0].backDirection = "up"
 	for j in range(len(y)):
 		E[0][j].data = j
+		E[0][j].coord = [0, j]
+		if (j != 0):
+			E[0][j].backPath = E[0][j - 1]
+		E[0][j].backDirection = "left"
+	# 	Loop through and get the numbers
 	for i in range(1, len(x)):
 		for j in range(1, len(y)):
 			subNumber = 0
@@ -84,14 +115,17 @@ def Edit(x, y, E):
 			if minimum == subNumber:
 				E[i][j].data = subNumber
 				E[i][j].backPath = E[i - 1][j - 1]
+				E[i][j].coord = [i, j]
 				E[i][j].backDirection = "diagonal"
 			elif minimum == addNumber:
 				E[i][j].data = addNumber
 				E[i][j].backPath = E[i][j - 1]
+				E[i][j].coord = [i, j]
 				E[i][j].backDirection = "left"
 			elif minimum == deleteNumber:
 				E[i][j].data = deleteNumber
 				E[i][j].backPath = E[i - 1][j]
+				E[i][j].coord = [i, j]
 				E[i][j].backDirection = "up"
 			else:
 				print("Something ain't right here")
@@ -107,3 +141,4 @@ class Node(object):
 		self.data = 0
 		self.backPath = None
 		self.backDirection = None
+		self.coord = [0, 0]
